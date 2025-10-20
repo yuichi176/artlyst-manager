0271b5db54863a424c0416bc784a20e0e3a31b1d
Yuichi Sugiyama <ozwald176@gmail.com>
2025-10-20 20:31:30 +0900

    chore: add Prettier and ESLint configurations, update project scripts

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -20,6 +21,7 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  eslintConfigPrettier,
 ];
 
 export default eslintConfig;
